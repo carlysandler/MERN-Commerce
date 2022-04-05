712cd7e27533869eeffc8c72438bbf2bfa57c3fd
Carly Sandler <carly.sandler@gmail.com>
2022-04-05 18:53:42 -0400

    Feat: adding passport-local middleware && strategy

--- server/src/middleware/gateKeepers.js
+++ server/src/middleware/gateKeepers.js
@@ -1,6 +1,28 @@
 const passport = require('passport')
 
-// require passport local strategy
+// require passport-local as middleware
+const requireLocalPassport = (req, res, next) => {
+	passport.authenticate("local", (err, user, info) => {
+	 if (err) {
+      return next(err);
+    }
+    if (info && info.message === "Missing credentials") {
+      return res.status(400).send(info, { message: "Missing credentials" });
+    }
+    if (!user) {
+      return res.status(400).send({ message: "Invalid email or password." });
+    }
+    if (!user.isVerified)
+      return res.status(401).send({
+        message: "Your account has not been verified. Please activate your account.",
+      });
+
+		req.user = user;
+		next()
+
+		})(req, res, next);
+
+}
 
 
 // require passport jwt strategy
